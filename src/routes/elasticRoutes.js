9d447e94622cb06b51cf8d908b57ec79b0487a3c
Luke Barsotti <luke.barsotti@gmail.com>
2017-06-20 19:51:06 -0400

    routes scaffolding

---
+++ src/routes/elasticRoutes.js
@@ -0,0 +1,25 @@
+
+
+elasticRoutes.posts['/api/data/likes/add'] = function(req, res) {
+
+}
+
+elasticRoutes.posts['/api/data/likes/delete'] = function(req, res) {
+
+}
+
+elasticRoutes.posts['/api/data/ideas/add'] = function(req, res) {
+
+}
+
+elasticRoutes.posts['/api/data/ideas/delete'] = function(req, res) {
+
+}
+
+elasticRoutes.posts['/api/data/ideas/search'] = function(req, res) {
+
+}
+
+elasticRoutes.posts['/api/data/idea/update'] = function(req, res) {
+  
+}
